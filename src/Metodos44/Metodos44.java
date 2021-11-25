f97a63f4508b2bbb0d5d52ef8dc15789cf07a862
92257852915 <artur.santos@serpro.gov.br>
2021-11-25 10:55:39 -0300

    metodos

---
+++ src/Metodos44/Metodos44.java
@@ -0,0 +1,11 @@
+package Metodos44;
+
+public class Metodos44 {
+    public void somaDoisNumeros(){
+        System.out.println(10+10);
+    }
+
+    public void subtraiDoisNumeros(){
+        System.out.println(20-10);
+    }
+}
---
+++ src/Metodos44/Metodos442.java
@@ -0,0 +1,20 @@
+package Metodos44;
+
+public class Metodos442 {
+    public static void main(String[] args) {
+        Metodos44 metodos44 = new Metodos44();
+        metodos44.somaDoisNumeros();
+        metodos44.subtraiDoisNumeros();
+       // System.out.println();
+        System.out.println("finish");
+    }
+}
+
+/*
+* Para criar atributos estabelece o modificador de acesso o tipo e o identificador.
+* Nos metodos ainda precida de um modificador de acesso, a segunda parte é o retorno.
+* A palavra void não retorna nada.
+* Em seguida o nome do  metodo.
+* Depois abre e fecha parenteses
+* depois abre e fecha chaves
+ */
